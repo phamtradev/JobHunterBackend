ebe55f5a1a316c01f1048c6d547a21da50368db0
Phạm Trà <phamvantra.dev@gmail.com>
2026-01-07 21:26:35 +0700

    feat: add github actions folder

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,15 +9,14 @@ group = "vn.phamtra"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 repositories {
     mavenCentral()
 }
-
-
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -28,17 +27,20 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+
     implementation("com.turkraft.springfilter:jpa:3.1.7")
     implementation("com.cloudinary:cloudinary-http44:1.33.0")
     implementation("org.apache.httpcomponents:httpclient:4.5.14")
+
+    runtimeOnly("com.mysql:mysql-connector-j")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    //runtimeOnly("com.mysql:mysql-connector-java")
-    implementation("mysql:mysql-connector-java:8.0.33")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
 
-tasks.withType<Test> {
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
 
@@ -46,7 +48,7 @@ tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
     archiveFileName.set("spring-boot-docker.jar")
 }
 
-tasks.withType<JavaExec> {
+tasks.withType<JavaExec>().configureEach {
     jvmArgs("--enable-native-access=ALL-UNNAMED")
 }
 
